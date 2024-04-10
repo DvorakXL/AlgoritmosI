233c4fa1258a27bcbe6c20c2ed40992b1e02b132
Qwerty <benjaminpondal@hotmail.com>
2024-04-10 12:35:36 -0300

    Agruegue hasta el ejercicio 4a

--- Haskell/ejercicios1.hs
+++ Haskell/ejercicios1.hs
@@ -1,21 +1,33 @@
 -- Ej 1a)
-f :: Integer -> Integer
+f :: Int -> Int
 f 1 = 8
 f 4 = 131
 f 16 = 16
 
 --Ej 1b)
-g :: Integer -> Integer
+g :: Int -> Int
 g 8 = 16
 g 16 = 4
 g 131 = 1
 
 --Ej 1c)
-h :: Integer -> Integer
+h :: Int -> Int
 h x = f(g x)
 
-k :: Integer -> Integer
+k :: Int -> Int
 k x = g(f x)
+
+---------------------------------------------------------
+
+--Ej 2a)
+absoluto :: Int -> Int
+absoluto x
+    | x >= 0 = x
+    | x < 0 = -x
+
+--Ej 2b)
+maximoabsoluto :: Int -> Int -> Int
+maximoabsoluto x y = max (absoluto x) (absoluto y)
 
 --Ej 2c)
 maximo3 :: Int -> Int -> Int -> Int
@@ -23,6 +35,26 @@ maximo3 x y z
     | x >= y && x >= z = x
     | y >= x && y >= z = y
     | otherwise = z
+
+--Ej 2d)
+algunoEs0 :: Float -> Float -> Bool
+algunoEs0 0 1 = True
+algunoEs0 1 0 = True
+algunoEs0 0 0 = True
+algunoEs0 x y = False
+
+--Ej 2e)
+ambosSon0 :: Float -> Float -> Bool
+ambosSon0 0 0 = True
+ambosSon0 x y = False
+
+--Ej 2f)
+mismoIntervalo :: Float -> Float -> Bool
+mismoIntervalo x y
+    | x <= 3 && y <=3 = True -- (-inf, 3]
+    | 3 < x && x <= 7 && 3 < y && y <= 7 = True -- (3,7]
+    | x > 7 && y > 7 = True-- (7, +inf)
+    | otherwise = False
 
 --Ej 2g)
 sumaDistintos :: Int -> Int -> Int -> Int
@@ -33,20 +65,26 @@ sumaDistintos x y z
     | x == y && x /= z && y /= z = z
     | otherwise = 0
 
+--Ej 2h)
+esMultiploDe :: Int -> Int -> Bool
+esMultiploDe x y = mod x y == 0 || mod y x == 0
+
 --Ej 2i)
-digitoUnidades :: Integer -> Integer
-digitoUnidades x
-    | x >= 0 = mod x 10
-    | otherwise = mod x (-10)
+digitoUnidades :: Int -> Int
+digitoUnidades x = mod (absoluto x) 10
 
 --Ej 2j)
-digitoDecenas :: Integer -> Integer
-digitoDecenas x
-    | x >= 0 = mod (div x 10) 10
-    | otherwise = -mod (div x 10) (-10)
+digitoDecenas :: Int -> Int
+digitoDecenas x = mod (div (absoluto x) 10) 10
+
+---------------------------------------------------------
+
+--Ej 3)
+estanRelacionados :: Int -> Int -> Bool
+estanRelacionados a b = a*a + a*b * div (-a) b == 0
+
+---------------------------------------------------------
 
 --Ej 4b)
 todoMenor :: (Float, Float) -> (Float, Float) -> Bool
-todoMenor t1 t2
-    | fst t1 < fst t2 && snd t1 < snd t2 = True
-    | otherwise = False
+todoMenor t1 t2 = fst t1 < fst t2 && snd t1 < snd t2
