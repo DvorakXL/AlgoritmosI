d782ebf3a1bfe721287ca909f641a7db5fcb56e7
benja <benjaminpondal@hotmail.com>
2024-06-01 22:31:16 -0300

    agregue ej 16

--- Python/guia8.py
+++ Python/guia8.py
@@ -2,6 +2,56 @@ from queue import LifoQueue as Pila
 from queue import Queue as Cola
 import random
 
+# Funciones auxiliares
+def print_pila (pila: Pila) -> None:
+    copia_pila: Pila = Pila()
+
+    while not pila.empty():
+        elemento = pila.get()
+        copia_pila.put(elemento)
+        print(elemento)
+    
+    # Restauro el estado previo de cola
+    mover_pila(pila, copia_pila)
+
+def print_cola (cola: Cola) -> None:
+    copia_cola: Cola = Cola()
+
+    while not cola.empty():
+        elemento = cola.get()
+        copia_cola.put(elemento)
+        print(elemento)
+    
+    # Restauro el estado previo de cola
+    mover_cola(cola, copia_cola)
+
+def vaciar_cola (cola: Cola) -> None:
+    while not cola.empty():
+        cola.get()
+
+def vaciar_pila (pila: Pila) -> None:
+    while not pila.empty():
+        pila.get()
+
+def mover_pila (hacia: Pila, desde: Pila) -> None:
+    # El parametro 'hacia' es de salida. Vaciamos la pila si tiene elementos
+    vaciar_pila(hacia)
+
+    # Esto vacia la pila 'desde' y pone sus elementos en 'hacia'
+    while not desde.empty():
+        elemento = desde.get()
+        hacia.put(elemento)
+
+def mover_cola (hacia: Cola, desde: Cola) -> None:
+    # El parametro 'hacia' es de salida. Vaciamos la cola si tiene elementos
+    vaciar_cola(hacia)
+
+    # Esto vacia la cola 'desde' y pone sus elementos en 'hacia'
+    while not desde.empty():
+        elemento = desde.get()
+        hacia.put(elemento)
+
+
 # Ejercicio 1.1
 def contar_lineas (nombre_archivo: str) -> int:
     archivo = open(nombre_archivo, 'r', encoding='utf-8')
@@ -133,12 +183,101 @@ def cola_numeros_al_azar (cantidad: int, desde: int, hasta: int) -> Cola[int]:
     pila_al_azar: Pila[int] = generar_nros_al_azar(cantidad, desde, hasta)
     cola_al_azar: Cola[int] = Cola()
 
-    for i in range(pila_al_azar.qsize()):
+    for i in range(cantidad_elementos(pila_al_azar)):
         cola_al_azar.put(pila_al_azar.get())
     
     return cola_al_azar
 
+# Ejercicio 14
+def cantidad_elementos_cola (cola: Cola) -> int:
+    copia_cola: Cola = Cola()
+    longitud = 0
 
-cola = cola_numeros_al_azar(5,5,20)
-for i in range(cola.qsize()):
-    print(cola.get())
+    # Suma uno a longitud por cada elemento dentro de la cola
+    while not cola.empty():
+        elemento = cola.get()
+        copia_cola.put(elemento)
+        longitud += 1
+
+    # Restauro el estado previo de la cola
+    mover_pila(cola, copia_cola)
+    
+    return longitud
+
+# Ejercicio 15
+def buscar_el_maximo_cola (cola: Cola[int]) -> int:
+    primer_valor = cola.get()
+    copia_cola: Cola = Cola()
+    copia_cola.put(primer_valor)
+    maximo = primer_valor
+
+    # Suma uno a longitud por cada elemento dentro de la cola
+    while not cola.empty():
+        elemento = cola.get()
+        copia_cola.put(elemento)
+
+        if elemento > maximo:
+            maximo = elemento
+
+    # Restauro el estado previo de la cola
+    mover_pila(cola, copia_cola)
+    
+    return maximo
+
+# Ejercicio 16
+def armar_secuencia_de_bingo () -> Cola[int]:
+    cola_numeros: Cola[int] = Cola()
+    lista_numeros: list[int] = []
+
+    # Genera los numeros del 0 al 99
+    for i in range(0,100):
+        lista_numeros.append(i)
+
+    # Escoge indices al azar
+    while not len(lista_numeros) == 0:
+        indice_al_azar = random.randint(0,len(lista_numeros)-1)
+        cola_numeros.put(lista_numeros[indice_al_azar])
+        lista_numeros.pop(indice_al_azar)
+    
+    return cola_numeros
+
+def generar_carton () -> list[int]:
+    carton: list[int] = []
+
+    while len(carton) < 12:
+        numero_generado: int = random.randint(0,99)
+        if not pertenece(carton, numero_generado):
+            carton.append(numero_generado)
+    
+    return carton
+
+# Supongo que piden hallar cuantos turnos para completar todo el carton
+def jugar_carton_de_bingo (carton: list[int], bolillero: Cola[int]) -> int:
+    copia_bolillero: Cola[int] = Cola()
+    copia_carton: list[int] = []
+    cantidad_jugadas: int = 0
+
+    while not len(carton) == 0:
+        bola: int = bolillero.get()
+        copia_bolillero.put(bola)
+
+        if pertenece(carton, bola):
+            copia_carton.append(bola)
+            carton.remove(bola)
+        cantidad_jugadas += 1
+
+    for elem in copia_carton:
+        carton.append(elem)
+    mover_cola(bolillero, copia_bolillero)
+
+    return cantidad_jugadas
+
+# ----------------- Testing -----------------------
+bingo = armar_secuencia_de_bingo()
+carton = generar_carton()
+print(carton)
+print_cola(bingo)
+print("A JUGAR!!")
+print(jugar_carton_de_bingo(carton, bingo))
+print("Estado final")
+print(carton)
